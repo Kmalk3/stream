a9a7a54b9b0fd35c6d065a9d4695b9fcf6fe6f67
Kmalk3 <44727595+Kmalk3@users.noreply.github.com>
2018-11-03 15:01:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+    client.user.setGame(`اصباعك على الزناد , وعينك علي الهدف `,'https://www.twitch.tv/MeeRcY')
+
+});
+client.login("NDcyNzYzMDc4MTAzMjY5Mzk2.DrS1ow.hfcnH58gM3uJFA4McQ1s51S8_fU")
